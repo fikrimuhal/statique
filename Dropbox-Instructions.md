aa22fc115f30da65a1eefa29506540f822a162d8
Ahmet Emre Aladağ <aladagemre@gmail.com>
2016-01-22 21:05:58 +0200

    Separating Hugo project and Markdown folders. Reading data folders from config/include.txt.

--- Dropbox-Instructions.md
+++ Dropbox-Instructions.md
@@ -1,5 +1,14 @@
 # Statique Dropbox Version
 
+
+## Dropbox Preparation
+
+* Make sure you have a folder called hugo-skeleton with a Hugo Project with empty content directory and Makefile in it.
+* Make sure you have a config/include.txt file with paths (directories) containing Markdown files to include in the build.
+* Make sure you have index.md files in each folder and \_index.md file at the root dropbox folder.
+
+## Statique Installation
+
 ```bash
 docker-compose up -d
 ```
@@ -8,12 +17,20 @@ or
 ```bash
 docker build -t fikrimuhal/statique-dropbox-base -f Dockerfile.base .
 docker build -t fikrimuhal/statique-dropbox -f Dockerfile.top .
-docker run -d --name statique-dropbox -e WEBHOOK_PATH="/abcd" -e DROPBOX_DOCS_REL_PATH="/docs" -p 3000:3000 fikrimuhal/statique-dropbox
+docker run -d --name statique-dropbox -e WEBHOOK_PATH="/abcd" -p 3000:3000 fikrimuhal/statique-dropbox
+```
+
+If a link doesn't appear, you may display the logs via
+```bash
+docker logs statique-dropbox
 ```
 
 * Navigate to the link to link your dropbox.
-* Exclude folders you don't want to sync.
+* Attach to the container and exclude folders you don't want to sync.
+
 ```bash
+docker exec -it statique-dropbox /bin/bash
+
 python /root/dropbox.py exclude add "/root/Dropbox/folder1"
 python /root/dropbox.py exclude add "/root/Dropbox/folder2"
 ```
--- build.cr
+++ build.cr
@@ -6,30 +6,48 @@ APP_LOCATION = __DIR__
 APP_PUBLIC   = File.join(APP_LOCATION, "/public")
 BUILT_PUBLIC = "/tmp/hugo-public"
 DROPBOX_PATH = File.join(ENV["HOME"], "Dropbox")
-# If you want to use root folder, use empty string.
-DOCS_REL_PATH = ENV["DROPBOX_DOCS_REL_PATH"]? ? ENV["DROPBOX_DOCS_REL_PATH"] as String : "/"
-DOCS_PATH     = File.join(DROPBOX_PATH, DOCS_REL_PATH)
-DOCS_MAKE     = File.join(DOCS_PATH, "/Makefile")
+HUGO_SKEL_PATH = File.join(DROPBOX_PATH, "hugo-skeleton")
+HUGO_PATH = "/tmp/hugo"
+HUGO_CONTENT_PATH = File.join(HUGO_PATH, "content")
+HUGO_MAKEFILE     = File.join(HUGO_PATH, "/Makefile")
 
 log = Logger.new(STDOUT)
 log.level = Logger::INFO
 log.info("App path: #{APP_LOCATION}")
 log.info("Dropbox path: #{DROPBOX_PATH}")
-log.info("Docs relative path: #{DOCS_PATH}")
-log.info("Makefile path: #{DOCS_MAKE}")
+log.info("Makefile path: #{HUGO_MAKEFILE}")
 
 unless File.exists?(DROPBOX_PATH)
   log.error("Dropbox folder does not exist. Make sure you have installed Dropbox.")
 end
 
-if File.exists?(DOCS_MAKE)
-  log.info("Makefile in Dropbox folder found, updating...")
-  Process.run("make", ["clean"], chdir: DOCS_PATH)
-  Process.run("make", ["html"], chdir: DOCS_PATH)
+# Copy Hugo project without content.
+Process.run("rm", ["-rf", HUGO_PATH])
+Process.run("cp", ["-rf", HUGO_SKEL_PATH, HUGO_PATH])
+
+def copy_dropbox_hugo_content(path)
+  if path.strip.size > 0
+    from_path = File.join(DROPBOX_PATH, path)
+    to_path = File.join(HUGO_CONTENT_PATH, path)
+    Process.run("cp", ["-rf", from_path, to_path])
+  end
+end
+
+# Copy Markdown content to Hugo project's content folder.
+include_paths = File.join(HUGO_PATH, "config", "include.txt")
+paths = File.read(include_paths).split("\n")
+paths.each { |path| copy_dropbox_hugo_content(path) }
+copy_dropbox_hugo_content("_index.md")
+
+
+if File.exists?(HUGO_MAKEFILE)
+  log.info("Makefile found, building...")
+  Process.run("make", ["clean"], chdir: HUGO_PATH)
+  Process.run("make", ["html"], chdir: HUGO_PATH)
   Process.run("rm", ["-rf", APP_PUBLIC])
-  Process.run("cp", ["-rf", BUILT_PUBLIC, APP_PUBLIC], chdir: DOCS_PATH)
-  Process.run("cp", ["-rf", "config", File.join(APP_LOCATION, "config")], chdir: DOCS_PATH)
+  Process.run("cp", ["-rf", BUILT_PUBLIC, APP_PUBLIC])
+  Process.run("cp", ["-rf", "config", File.join(APP_LOCATION, "config")], chdir: HUGO_PATH)
   log.info("Build complete. Output is on #{BUILT_PUBLIC}".colorize(:green))
 else
-  log.info("Makefile in Dropbox folder not found. Please provide Makefile.")
+  log.info("Makefile in Dropbox hugo-skeleton folder not found. Please provide Makefile.")
 end
