35f1327d2fedcd35d487ed61cbb7be26b8499429
Ahmet Emre Aladağ <aladagemre@gmail.com>
2016-01-29 15:59:01 +0000

    templates -> defaults

--- inotifyrun.sh
+++ inotifyrun.sh
@@ -11,16 +11,32 @@ build_after_uptodate() {
     sleep 1
   done
   # When it is fully synced, start build.
-  if [ -f $HOME/Dropbox/statique/build.py ]; then
-    cp $HOME/Dropbox/statique/build.py .
-  elif [ -f ./templates/build.py ]; then
-    cp ./templates/build.py .
+
+  DROPBOX_BUILD=$HOME/Dropbox/statique/build.py
+  DEFAULT_BUILD=./defaults/build.py
+
+  if [ -f $DROPBOX_BUILD ]; then
+    cp $DROPBOX_BUILD .
+  elif [ -f $DEFAULT_BUILD ]; then
+    cp $DEFAULT_BUILD .
   fi
 
-  if [ -f $HOME/Dropbox/statique/generate_index.py ]; then
-    cp $HOME/Dropbox/statique/generate_index.py .
-  elif [ -f ./templates/generate_index.py ]; then
-    cp ./templates/generate_index.py .
+  DROPBOX_GI=$HOME/Dropbox/statique/generate_index.py
+  DEFAULT_GI=./defaults/generate_index.py
+
+  if [ -f $DROPBOX_GI ]; then
+    cp $DROPBOX_GI .
+  elif [ -f $DEFAULT_GI ]; then
+    cp $DEFAULT_GI .
+  fi
+
+  DROPBOX_MD2HTML=$HOME/Dropbox/statique/md2html.sh
+  DEFAULT_MD2HTML=./defaults/md2html.sh
+
+  if [ -f  ]; then
+    cp $DROPBOX_MD2HTML .
+  elif [ -f $DEFAULT_MD2HTML ]; then
+    cp $DEFAULT_MD2HTML .
   fi
 
   echo "Starting build..."
